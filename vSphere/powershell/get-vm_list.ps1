9d201d38bd83ad88bdf1de738592fe68b95bd743
florinen <nenfl@yahoo.com>
2019-12-23 21:39:40 -0600

    scripts for snapshot lifecycle in vspaere

---
+++ vSphere/powershell/get-vm_list.ps1
@@ -0,0 +1,74 @@
+
+<#
+Script name: Get-VMpowerstate_AndHostStatus.ps1
+Created on: 09/18/2016
+Author: Cory Barker, aka CaptainvOps, @corybark3r
+Description: Collects powerstate of VMs, which host they are running on, the VM IP and cluster.  Writes output to CSV. Creates WIN system event log based on completion, and will delete said CSV after 7 days. This was designed to be an automated task.
+===Tested Against Environment====
+vSphere Version: 5.5.0.2442329
+PowerCLI Version: PowerCLI 5.8
+PowerShell Version: 5.8
+OS Version: Windows 10/2008R2
+#>
+
+#variables
+$vCenter = @()
+$sites = @("vcsa")
+$domain = @("varu.local")
+$user = @("florin@varu.local")
+$password = Get-Content "C:\Users\Desktop\esxi\encrypted_pass\my_pass.txt" | ConvertTo-SecureString 
+$credential = New-Object System.Management.Automation.PsCredential($user,$password)
+#get array of sites and establishes connections to vcenters
+foreach ($site in $sites) {
+  	$vCenter = $site + "." + $domain
+
+	#If you're not using a powershell credentials file to pass encrypted credentials to this script, uncomment -User <username> -Password <password>
+	
+  	Connect-VIServer -Server $vCenter -Credential $credential  #-User <username> -Password <password>
+
+	$vmhosts=get-view -ViewType hostsystem -Property name,parent
+	$hostshash=@{}
+	$vmhosts|%{$hostshash.Add($_.moref.toString(),$_.name)}
+	$clusters=get-view -viewtype ClusterComputeResource -Property name
+	$clustershash=@{}
+	$clusters|%{$clustershash.Add($_.moref.toString(),$_.name)}
+	$hoststoclusterhash=@{}
+	$vmhosts|%{$hoststoclusterhash.add($_.moref.toString(),$clustershash.($_.Parent.ToString()))}
+	$vms=get-view -viewtype virtualmachine -property name, runtime.host, runtime.powerstate, guest.hostname, guest.net
+    
+    $report = $vms | ForEach-Object {
+	 $ips = @()
+	 $_.guest.net.ipaddress | ForEach-Object {
+	   if ($_) {
+	     $ips += $_
+	   }
+	 }
+
+	 [PSCustomObject]@{
+	   "Host" = $hostshash.($_.Runtime.Host.ToString())
+	   "Cluster" = $hoststoclusterhash.($_.Runtime.Host.ToString())
+	   "Name" = $_.name
+	   "DNS Host Name" = $_.guest.hostname
+	   "Power State" = $_.runtime.powerstate
+	   "Ip Addresses" = $ips -join ', '
+	 }
+	}
+	#send output to csv and disconnect from vcenter
+	$report | Sort-Object Host | export-csv -path "C:\Users\Desktop\esxi\logs\$site $((Get-Date).ToString('MM-dd-yyyy_hhmm')).csv" -NoTypeInformation -UseCulture
+	
+	Disconnect-VIServer -Confirm:$false -Server $vCenter
+	
+	<#
+	Write event log on completetion per site.  
+	note: if running for first time you will need to run powershell as administrator and run commands:
+	'New-EventLog –LogName Application –Source Get-VMpowerstate_AndHostStatus'
+	'Write-EventLog -LogName Application -Source Get-VMpowerstate_AndHostStatus -EventId 007 -EntryType Information -Message "Get-VMpowerstate_AndHostStatus for vCenter $site completed successfully."'
+	#>
+	
+	Write-EventLog -LogName Application -Source Get-VMpowerstate_AndHostStatus -EventId 007 -EntryType Information -Message "Get-VMpowerstate_AndHostStatus for vCenter $site completed successfully."
+}
+
+#Cleanup old csv after 7 days.  Delete files older than the limit
+ $limit = (Get-Date).AddDays(-7)
+ $path = "C:\Users\Desktop\esxi\logs\"
+Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } #| Remove-Item -Force
