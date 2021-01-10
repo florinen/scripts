2fa4b3a4d34e12054ffaa6b77c03150a7732a267
florinen <nenfl24@gmail.com>
2021-01-10 20:04:40 +0000

    updating script to backup nctest too

--- nc_backup/backup_cloud.sh
+++ nc_backup/backup_cloud.sh
@@ -8,38 +8,60 @@ RESET=`tput sgr0`
 
 
 CURRDATE=` date '+%m-%d-%Y' `
-HOSTNAME="freenas.varu.local"
-DB_USER="root"
+REMOTE_HOST="freenas.varu.local"
 REMOTE_USER="nc_user"
-DB_Name="nextcloud"
+
+# Can get the DB name from mysql:
+#DB_NAME=$(mysql -u $DB_USER -p$DB_PASS -e 'show databases;' |grep dDB)
+## Backing up for a speciffic user: 
+#DB_USER=$(cat /var/www/nextcloud/config/config.php |grep dbuser |cut -d'>' -f2 |sed "s/[ '\,]//g")
+
+# If you dont know the current DB credentials:
+DB_NAME=$(cat /var/www/nextcloud/config/config.php |grep dbname |cut -d'>' -f2 |sed "s/[ '\,]//g")
+## Backing up all DB's available in mysql using root USER: 
+DB_USER="root"
+DB_PASS=$(cat /var/www/nextcloud/config/config.php |grep dbpassword |cut -d'>' -f2 |sed "s/[ '\,]//g")
+
+NC_VERSION=$(sudo -u www-data php /var/www/nextcloud/occ -V |awk '{print $NF}')
 NC_FOLDER="/var/www/nextcloud"
 DEST_LOCAL="$HOME/nc_backups"
-DEST_REMOTE="$HOSTNAME:/mnt/Storage/nfs/Nextcloud/nc_backups/nc_user/backups"
-DB_PASS=`ssh  $REMOTE_USER"@"$HOSTNAME "cat /mnt/Storage/nfs/Nextcloud/nc_backups/nc_user/.my*"`
+DEST_REMOTE="$REMOTE_HOST:/mnt/Storage/nfs/Nextcloud/nc_backups/nc_user/backups"
+#DB_PASS=`ssh  $REMOTE_USER"@"$REMOTE_HOST "cat /mnt/Storage/nfs/Nextcloud/nc_backups/nc_user/.my*"`
 
+if [ -d  $NC_FOLDER ]; then
+            
+    # test if database folder exists
+    if [ ! -d $DEST_LOCAL ]; then
+        echo "Backup folder doesn't exists on your filesystem."
+        mkdir -p $HOME/nc_backups
+        if [ -d $HOME/nc_backups ]; then
+            echo "Folder ${DEST_LOCAL} was created.."
+        else 
+            echo "Backup folder could not be created - ERROR!"
+        fi
+    fi 
+fi     
 echo "====>$YELLOW Backing up NextCloud database...!! $RESET<===="
-mysqldump -u $DB_USER -p$DB_PASS -C $DB_Name > $DEST_LOCAL"/"$DB_Name"_"$CURRDATE.sql.tgz
+mysqldump -u $DB_USER -p$DB_PASS -C $DB_NAME &>/dev/null > $DEST_LOCAL"/"$HOSTNAME"_"$DB_NAME"_"$CURRDATE"_v"$NC_VERSION.sql.tgz
 echo "$GREEN done!! $RESET"
 sleep 3
 echo "====>$YELLOW Saving $NC_FOLDER folder - as tar file... $RESET<===="
-tar -cpvzf $DEST_LOCAL"/"$DB_Name"_"$CURRDATE.tar.gz $NC_FOLDER
+tar -cpvzf $DEST_LOCAL"/"$HOSTNAME"_"$DB_NAME"_"$CURRDATE"_v"$NC_VERSION.tar.gz $NC_FOLDER
 echo ""
 echo "$GREEN done!! $RESET"
 echo ""
-echo "Rsync ran today backing up : $DB_Name"_"$CURRDATE.sql  and   $DB_Name"_"$CURRDATE.tar.gz at $(date)" >> $DEST_LOCAL"/"$DB_Name-backup.log 2>&1
+echo "Rsync ran today backing up : $HOSTNAME"_"$DB_NAME"_"$CURRDATE"_v"$NC_VERSION.sql  and   $HOSTNAME"_"$DB_NAME"_"$CURRDATE"_v"$NC_VERSION.tar.gz at $(date)" >> $DEST_LOCAL"/"$HOSTNAME"_"$DB_NAME-backup.log 2>&1
 echo "=====>$YELLOW Transfer NextCloud backups to remote $RESET<===="
 rsync -avzP  $DEST_LOCAL"/" $REMOTE_USER"@"$DEST_REMOTE
 echo ""
 echo "$GREEN Transfer done!! $RESET"
 echo ""
 echo "====>$YELLOW Cleanning up $DEST_LOCAL..!! $RESET<===="
-rm -fv $DEST_LOCAL"/"$DB_Name*
+rm -fv $DEST_LOCAL"/"$HOSTNAME*
 echo ""
 echo "$GREEN done!! $RESET"
 echo ""
-echo "Rsync ran today backing up :" $DB_Name"_"$CURRDATE.sql " and " $DB_Name"_"$CURRDATE.tar.gz " at  $(date)"
+echo "Rsync ran today backing up :" $HOSTNAME"_"$DB_NAME"_"$CURRDATE"_v"$NC_VERSION.sql " and " $HOSTNAME"_"$DB_NAME"_"$CURRDATE"_v"$NC_VERSION.tar.gz " at  $(date)"
 echo "$GREEN Backup COMPLETE!! $RESET"
-echo ""
-
 exit
 
