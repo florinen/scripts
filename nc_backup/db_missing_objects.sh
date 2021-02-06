10c8ccd9fbde01118b8b3d00394b4eaadc9d4419
florinen <nenfl24@gmail.com>
2021-02-06 00:56:00 +0000

    updated cmd

--- nc_backup/db_missing_objects.sh
+++ nc_backup/db_missing_objects.sh
@@ -2,12 +2,15 @@
 
 ##Some DB missing opjects. After testing add all extra CMD's here:
 ## Executed by upgrade_nc.sh
-BD_OBJECTS=$(add-missing-indices add-missing-columns add-missing-primary-keys convert-filecache-bigint)
-sudo -u www-data php /var/www/nextcloud/occ db:add-missing-indices
-sudo -u www-data php /var/www/nextcloud/occ db:add-missing-columns
-sudo -u www-data php /var/www/nextcloud/occ db:add-missing-primary-keys
-sudo -u www-data php /var/www/nextcloud/occ db:convert-filecache-bigint << EOF
-y
-EOF
+## Add all missing objects below:
+BD_OBJECTS="add-missing-indices add-missing-columns add-missing-primary-keys convert-filecache-bigint"
 echo ""
 
+# Fixing DB errors after upgrade.
+for object in $(echo "${BD_OBJECTS}" |xargs -n1 )
+do
+    sudo -u www-data php /var/www/nextcloud/occ db:"${object}" << EOF
+y
+Y
+EOF
+done
