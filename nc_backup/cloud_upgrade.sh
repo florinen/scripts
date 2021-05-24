b90e3fb355b23e5d17ba2db6fe069a807fd67b08
florinen <nenfl24@gmail.com>
2021-05-24 18:12:30 -0500

    added status fn

--- nc_backup/cloud_upgrade.sh
+++ nc_backup/cloud_upgrade.sh
@@ -6,14 +6,28 @@ YELLOW=$(tput setaf 3)
 MAGENTA=$(tput setaf 5)
 RESET=$(tput sgr0)
 
+STATUS () {
+    echo "$?"
+}
+
 echo "Date is: ${GREEN}`date`${RESET}"
 echo "You are login as: ${GREEN}`whoami`${RESET}"
 echo ""
-echo "${GREEN}Before update NC will backup DB first...${RESET}"
 
-/bin/bash $HOME/scripts/nc_backup/backup_cloud.sh &> >(grep -E "done!!|COMPLETE!!") 
+# Executing DB backup script
+echo -e "${GREEN}Before update NC will backup DB first... \nBacking up DB...${RESET}"
+/bin/bash $(locate -i backup_cloud.sh) &> >(grep -E 'done!!|COMPLETE!!')
+
+if [[ "$(STATUS)" != "0" ]]; then
+    echo "${RED}Backing up NC DB failed.!!${RESET}"
+fi
 sleep 2
-echo ""
-echo "${GREEN}Upgrading NC starts now...${RESET}"
 
-/bin/bash $HOME/scripts/nc_backup/upgrade_nc.sh
+# Executinh Upgrade NC script
+echo -e "${GREEN}Upgrading NC starts now... \nUpgrading...${RESET}"
+/bin/bash $(locate -i upgrade_nc.sh)
+
+if [[ "$(STATUS)" != "0" ]]; then
+    echo "${RED}Upgrading NC failed.!!${RESET}"
+fi
+
